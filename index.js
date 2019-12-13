ff33d5de729a804e4c5f43e153e96adf36366ddb
Torin Blankensmith <torin.blankensmith@gmail.com>
2019-12-13 15:59:00 -0500

    fix cam position

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ import { PMREMGenerator } from 'three/examples/jsm/pmrem/PMREMGenerator.js';
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+let initialCameraZ = 200;
+camera.position.z = initialCameraZ;
 window.camera = camera;
+
 const container = document.querySelector('.canvas-container');
 let renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, physicallyCorrectLights: true});
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -37,32 +40,28 @@ controls.zoomSpeed =  0.5;
 controls.rotateSpeed =  0.5;
 controls.enableZoom = false;
 
-let initialCameraZ = 200;
-let yOffset = 0;
+
 let endOffset = document.querySelector('.end-intro').offsetTop;
 
 document.addEventListener('scroll', (e) => {
-    let zoom = controls.getZoomScale();
-    camera.position.z = Math.max(initialCameraZ - initialCameraZ * (window.pageYOffset / endOffset), 0);
+    // let zoom = controls.getZoomScale();
+    camera.position.z = Math.max(initialCameraZ - initialCameraZ * (window.pageYOffset / endOffset), 0.01);
 }, false);
 window.controls = controls;
 let mouseX = 0, mouseY = 0;
-// let windowHalfX = window.innerWidth / 2;
-// let windowHalfY = window.innerHeight / 2;
-let group;
 
 
 init();
 animate();
 
+let group;
 function init() {
     group = new THREE.Group();
     let geometry = new THREE.IcosahedronBufferGeometry(10, 0);
     let material = new THREE.MeshStandardMaterial({ color: 0x550000, metalness: 0.2, shading: THREE.FlatShading });
     material.roughness = 0.3;
-    // Instantiate a loader
-    let loader = new GLTFLoader();
 
+    let loader = new GLTFLoader();
     // Optional: Provide a DRACOLoader instance to decode compressed mesh data
     // let dracoLoader = new THREE.DRACOLoader();
     // dracoLoader.setDecoderPath('/examples/js/libs/draco/');
@@ -81,13 +80,14 @@ function init() {
         (xhr) => console.log((xhr.loaded / xhr.total * 100) + '% loaded'),
         (error) => console.log('An error happened'));    
 
-    for (let i = 0; i < 10000; i++) {
+    for (let i = 0; i < 100; i++) {
         let mesh = new THREE.Mesh(geometry, material);
-        mesh.position.x = Math.random() * 2000 - 1000;
-        mesh.position.y = Math.random() * 2000 - 1000;
-        mesh.position.z = Math.random() * 2000 - 1000;
+        mesh.position.x = Math.random()*100 * (Math.round(Math.random()) ? -1 : 1);
+        mesh.position.y = Math.random()*100 * (Math.round(Math.random()) ? -1 : 1);
+        mesh.position.z = Math.random()*100 * (Math.round(Math.random()) ? -1 : 1);
         mesh.rotation.x = Math.random() * 2 * Math.PI;
         mesh.rotation.y = Math.random() * 2 * Math.PI;
+        mesh.rotation.z = Math.random() * 2 * Math.PI;
         mesh.matrixAutoUpdate = false;
         mesh.updateMatrix();
         group.add(mesh);
@@ -97,16 +97,13 @@ function init() {
     let light2 = new THREE.PointLight({ color: 0x004455 });
     scene.add(light, light2, group);
     renderer.sortObjects = false;
-    camera.position.z = initialCameraZ;
 }
-
 
 function lerp(v0, v1, t) {
     return v0 * (1 - t) + v1 * t
 }
 
 function onDocumentMouseMove(event) {
-    // console.log('mouse move')
     mouseX = (event.clientX) / window.innerWidth;
     mouseY = (event.clientY) / window.innerHeight;
     let rx = Math.sin(mouseX ) * 0.5;
@@ -123,10 +120,8 @@ function animate() {
 
 function render() {
     let time = Date.now() * 0.001;
-
     controls.update();
     camera.lookAt(scene.position);
-
     renderer.render(scene, camera);
 }
 
